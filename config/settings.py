d8adba25938b7427ffbf2a2c6a7f8437e993ac2e
yahiaAlg <103883836+yahiaAlg@users.noreply.github.com>
2025-05-02 12:31:41 +0100

    `Added additional template directory to TEMPLATES setting`

--- config/settings.py
+++ config/settings.py
@@ -58,7 +58,10 @@ ROOT_URLCONF = "config.urls"
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": [os.path.join(BASE_DIR, "pages", "templates")],
+        "DIRS": [
+            os.path.join(BASE_DIR, "pages", "templates"),
+            os.path.join(BASE_DIR, "templates"),
+        ],
         "APP_DIRS": True,
         "OPTIONS": {
             "context_processors": [
